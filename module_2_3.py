4345af3d196239ae319d57d889648ebe004e6883
Meteor117 <kharitonov_62@mail.ru>
2024-08-26 19:10:23 +0300

    Add files via upload

---
+++ module_2_3.py
@@ -0,0 +1,13 @@
+module_2_3.py
+
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+while i < len(my_list):
+    if my_list[i] > 0:
+        print(my_list[i])
+    elif my_list[i] < 0:
+        break
+        continue
+    i += 1
+
+
